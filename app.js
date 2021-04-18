aab6b455f9236b694ad1866587d8ae3a26d59992
yasey <ydeeyagahawadug@deakin.edu.au>
2021-04-18 19:41:00 +1000

    connect mongodb database

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const favicon = require('serve-favicon');
 const path = require('path');
 
 // create express app
 const app = express();
 const port = 3000;
+
+// connect to mongodb & listen for requests
+const uri = 'mongodb://localhost/automotive-intelligence';
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(uri, options).then(() => {
+    console.log("Databse connected");
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
 
 // set view engine
 app.set('view engine', 'ejs');
@@ -17,8 +31,4 @@ app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 
 app.get('/', (req, res) => {
     res.render('index');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
