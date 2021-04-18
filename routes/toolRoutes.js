898c59eaa85785600de5e7c1a654fddbac6c8815
yasey <ydeeyagahawadug@deakin.edu.au>
2021-04-18 23:29:43 +1000

    implement tool route handlers

--- routes/toolRoutes.js
+++ routes/toolRoutes.js
@@ -6,6 +6,11 @@ const toolController = require('../controllers/toolController');
 const router = express.Router();
 
 // tool routes
+router.get('/add', toolController.getAddTool);
+router.get('/', toolController.getAllTools);
+router.post('/add', toolController.postAddTool);
+router.get('/:id', toolController.getTool);
+router.delete('/:id', toolController.deleteTool);
 
 // export router handles
 module.exports = router;
